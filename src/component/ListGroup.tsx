343b3ac45b677a59adeceb5ae588330c82ed28f6
vasconcelosjean42 <vasconcelosjean@acad.ifma.edu.br>
2023-11-20 11:04:38 -0300

    conditional

---
+++ src/component/ListGroup.tsx
@@ -0,0 +1,20 @@
+const ListGroup = () => {
+  let items = ["Brazil", "Maranhão", "São Paulo", "Rio de Janeiro", "Salvador"];
+
+  items = [];
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 ? <p>No item found</p> : null}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item" key={item}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default ListGroup;
