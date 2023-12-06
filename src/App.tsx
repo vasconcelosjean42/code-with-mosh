6288063192aa8204ff7958b0290f5d04a907e2eb
vasconcelosjean42 <vasconcelosjean@acad.ifma.edu.br>
2023-12-05 21:47:58 -0300

    lesson 12

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,40 @@ import produce from "immer";
 import NavBar from "./component/NavBar";
 import Cart from "./component/Cart";
 
+interface ItemProps {
+  id: number;
+  title: string;
+  quantity: number;
+}
+
 const App = () => {
-  const [cartItems, setCartItems] = useState(["Product 1", "Product 2"]);
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
+  });
 
   const handleClick = () => {
-    setCartItems((prevState) => [
-      ...prevState,
-      `Product ${prevState.length + 1}`,
-    ]);
+    setCart({
+      ...cart,
+      items: [...cart.items].map((item: ItemProps) =>
+        item.id === 1 ? { ...item, quantity: 2 } : item
+      ),
+    });
+
+    setCart(
+      produce((draft) => {
+        const item = draft.items.find((item: ItemProps) => item.id === 1);
+        if (item) item.quantity = 2;
+      })
+    );
   };
 
   return (
     <>
-      <NavBar cartItemsCount={cartItems.length} />
-      <Cart
-        cartItems={cartItems}
-        onClick={handleClick}
-        onClear={() => setCartItems([])}
-        onRemove={(product: string) =>
-          setCartItems((prevState) =>
-            prevState.filter((item) => item !== product)
-          )
-        }
-      />
+      <button onClick={handleClick}>Mudar o nome</button>
     </>
   );
 };
