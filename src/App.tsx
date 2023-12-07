7801b7267ae46a6132b37bcece984ee5ae65bf7e
vasconcelosjean42 <vasconcelosjean@acad.ifma.edu.br>
2023-12-06 21:12:11 -0300

    lesson 6 exercice 13

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,28 @@
-import { useState } from "react";
-import produce from "immer";
-import NavBar from "./component/NavBar";
-import Cart from "./component/Cart";
-
-interface ItemProps {
-  id: number;
-  title: string;
-  quantity: number;
-}
+import ExpandableText from "./component/ExpandableText";
 
 const App = () => {
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      { id: 1, title: "Product 1", quantity: 1 },
-      { id: 2, title: "Product 2", quantity: 1 },
-    ],
-  });
-
-  const handleClick = () => {
-    setCart({
-      ...cart,
-      items: [...cart.items].map((item: ItemProps) =>
-        item.id === 1 ? { ...item, quantity: 2 } : item
-      ),
-    });
-
-    setCart(
-      produce((draft) => {
-        const item = draft.items.find((item: ItemProps) => item.id === 1);
-        if (item) item.quantity = 2;
-      })
-    );
-  };
-
   return (
     <>
-      <button onClick={handleClick}>Mudar o nome</button>
+      <ExpandableText maxChars={100}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor rem
+        necessitatibus odit hic adipisci quam, quis facilis ea sequi cum
+        consequuntur maiores. Amet incidunt dolore nemo quas ut dolores
+        est.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor rem
+        necessitatibus odit hic adipisci quam, quis facilis ea sequi cum
+        consequuntur maiores. Amet incidunt dolore nemo quas ut dolores
+        est.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor rem
+        necessitatibus odit hic adipisci quam, quis facilis ea sequi cum
+        consequuntur maiores. Amet incidunt dolore nemo quas ut dolores est.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor rem
+        necessitatibus odit hic adipisci quam, quis facilis ea sequi cum
+        consequuntur maiores. Amet incidunt dolore nemo quas ut dolores
+        est.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor rem
+        necessitatibus odit hic adipisci quam, quis facilis ea sequi cum
+        consequuntur maiores. Amet incidunt dolore nemo quas ut dolores
+        est.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor rem
+        necessitatibus odit hic adipisci quam, quis facilis ea sequi cum
+        consequuntur maiores. Amet incidunt dolore nemo quas ut dolores est.
+      </ExpandableText>
     </>
   );
 };
