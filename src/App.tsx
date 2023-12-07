2b2f679c97160ae1e30d45ab9fdff4e0256405b6
vasconcelosjean42 <vasconcelosjean@acad.ifma.edu.br>
2023-12-06 21:53:02 -0300

    uncontroled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,9 @@
-import ExpandableText from "./component/ExpandableText";
+import Form from "./Form";
 
 const App = () => {
   return (
     <>
-      <ExpandableText maxChars={100}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor rem
-        necessitatibus odit hic adipisci quam, quis facilis ea sequi cum
-        consequuntur maiores. Amet incidunt dolore nemo quas ut dolores
-        est.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor rem
-        necessitatibus odit hic adipisci quam, quis facilis ea sequi cum
-        consequuntur maiores. Amet incidunt dolore nemo quas ut dolores
-        est.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor rem
-        necessitatibus odit hic adipisci quam, quis facilis ea sequi cum
-        consequuntur maiores. Amet incidunt dolore nemo quas ut dolores est.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor rem
-        necessitatibus odit hic adipisci quam, quis facilis ea sequi cum
-        consequuntur maiores. Amet incidunt dolore nemo quas ut dolores
-        est.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor rem
-        necessitatibus odit hic adipisci quam, quis facilis ea sequi cum
-        consequuntur maiores. Amet incidunt dolore nemo quas ut dolores
-        est.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor rem
-        necessitatibus odit hic adipisci quam, quis facilis ea sequi cum
-        consequuntur maiores. Amet incidunt dolore nemo quas ut dolores est.
-      </ExpandableText>
+      <Form></Form>
     </>
   );
 };
