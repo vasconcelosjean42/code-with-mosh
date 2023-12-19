16c6401deed9dd9198f6d31e6a218aac5777119d
vasconcelosjean42 <vasconcelosjean@acad.ifma.edu.br>
2023-12-19 20:38:56 -0300

    fetching data and handling errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,46 @@
-import { useState } from "react";
-import ProductList from "./component/ProductList";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+type UserProps = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
 
 const App = () => {
-  const [category, setCategory] = useState("");
+  const [users, setUsers] = useState<UserProps[]>([]);
+  const [error, setError] = useState("");
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/xusers")
+      .then((res) => setUsers(res.data))
+      .catch((err: { message: string }) => setError(err.message));
+  }, []);
 
   return (
     <>
-      <select
-        className="form-select"
-        onChange={(e) => setCategory(e.target.value)}
-      >
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      <ProductList category={category} />
+      {error && <div className="alert alert-danger">{error}</div>}
+      {users.map((user) => (
+        <p>{user.name}</p>
+      ))}
     </>
   );
 };
