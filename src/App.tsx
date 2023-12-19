d8927a7814dbb52d74704f9073a5fe5bcee7fedb
vasconcelosjean42 <vasconcelosjean@acad.ifma.edu.br>
2023-12-19 19:55:16 -0300

    three lessons about useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { FormEvent, useState } from "react";
-import Form from "./component/expense-tracker/Form";
-import List from "./component/expense-tracker/List";
-
-export interface itemProps {
-  description: string;
-  amount: number;
-  category: string;
-}
+import { useState } from "react";
+import ProductList from "./component/ProductList";
 
 const App = () => {
-  const [item, setItem] = useState<itemProps[]>();
-
-  const onSubmit = (data: itemProps) => {
-    const newItem = {
-      description: data.description,
-      amount: data.amount,
-      category: data.category,
-    };
-    setItem((prevState) => (prevState ? [...prevState, newItem] : [newItem]));
-  };
-
-  const handleDelete = (index: number) => {
-    setItem((prevState) => {
-      if (prevState) return [...prevState].filter((item, i) => i !== index);
-    });
-  };
+  const [category, setCategory] = useState("");
 
   return (
     <>
-      <Form onSubmit={onSubmit}></Form>
-      {item ? <List items={item} onDelete={handleDelete}></List> : null}
+      <select
+        className="form-select"
+        onChange={(e) => setCategory(e.target.value)}
+      >
+        <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="Household">Household</option>
+      </select>
+      <ProductList category={category} />
     </>
   );
 };
