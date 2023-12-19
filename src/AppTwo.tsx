01f35ee017f47d24cbdda2a69ee4adc7b656aff7
vasconcelosjean42 <vasconcelosjean@acad.ifma.edu.br>
2023-12-19 20:39:30 -0300

    two changes

---
+++ src/AppTwo.tsx
@@ -0,0 +1,37 @@
+import { FormEvent, useState } from "react";
+import Form from "./component/expense-tracker/Form";
+import List from "./component/expense-tracker/List";
+
+export interface itemProps {
+  description: string;
+  amount: number;
+  category: string;
+}
+
+const AppTwo = () => {
+  const [item, setItem] = useState<itemProps[]>();
+
+  const onSubmit = (data: itemProps) => {
+    const newItem = {
+      description: data.description,
+      amount: data.amount,
+      category: data.category,
+    };
+    setItem((prevState) => (prevState ? [...prevState, newItem] : [newItem]));
+  };
+
+  const handleDelete = (index: number) => {
+    setItem((prevState) => {
+      if (prevState) return [...prevState].filter((item, i) => i !== index);
+    });
+  };
+
+  return (
+    <>
+      <Form onSubmit={onSubmit}></Form>
+      {item ? <List items={item} onDelete={handleDelete}></List> : null}
+    </>
+  );
+};
+
+export default AppTwo;
